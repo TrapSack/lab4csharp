62e18fc5d59ec3f20712125c476da92efbdebdfe
TrapSack <61499327+TrapSack@users.noreply.github.com>
2020-12-14 21:39:37 +0300

    Add files via upload

---
+++ DataManager/DataManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransferProtocolLibrary.DataManagement.Database;
+using TransferProtocolLibrary.DataManagement.Parsers;
+
+namespace TransferProtocolLibrary.DataManagement
+{
+    public class DataManager
+    {
+        public static Action<string> OnFileCreation;
+
+        private IXMLGenerator stringGenerator;
+        public DatabaseManager databaseManager;
+
+        private string targetPath;
+
+        public DataManager(IConfigDatabase config)
+        {
+            databaseManager = new DatabaseManager(config.GetConnectionString(), config.GetDatabaseName());
+
+            stringGenerator = new XMLGenerator();
+
+            targetPath = config.GetTargetForDataManager();
+        }
+
+        public void CreateXMLFiles()
+        {
+            List<string> tableNames = databaseManager.GetAllTablesNames();
+            
+            foreach (var tableName in tableNames)
+            {
+                string fileText = stringGenerator.GetXMLTableString(tableName,
+                    databaseManager.GetTableValues(tableName));
+
+                WriteXMLFile(tableName, fileText);
+
+                OnFileCreation?.Invoke($"Successfully created {tableName}.xml at {targetPath}");
+            }
+        }
+
+        private void WriteXMLFile(string tableName, string fileText)
+        {
+            tableName = DeleteScemaFromName(tableName);
+
+            string targetFile = Path.Combine(targetPath, tableName);
+            targetFile = Path.ChangeExtension(targetFile, ".xml");
+
+            using (StreamWriter sw = File.CreateText(targetFile))
+            {
+                sw.WriteLine(fileText);
+            }
+        }
+
+        private string DeleteScemaFromName(string name)
+        {
+            int dotIndex = name.IndexOf('.');
+            name = name.Substring(dotIndex+1);
+            return name;
+        }
+
+    }
+}
---
+++ LogManager/LogManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using TransferProtocolLibrary.DataManagement;
+using TransferProtocolLibrary.DataManagement.Database;
+
+namespace TransferProtocolLibrary.Logging
+{
+    public class LogManager : ILogger
+    {
+        DatabaseManager databaseManager;
+
+        private const string tableName = "Errors";
+
+        private string[] columnsTypes = { "Time varchar(50)", "Message varchar(100)" };
+        private string[] columnsNames = { "Time", "Message" };
+
+        public LogManager(DatabaseManager databaseManager)
+        {
+            this.databaseManager = databaseManager;
+
+            if (!databaseManager.GetAllTablesNames().Contains("dbo." + tableName))
+            {
+                databaseManager.CreateTable(tableName, columnsTypes);
+            }
+        }
+
+        public void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            LogWithTime(message);
+            LogDatabase(message);
+            Console.ResetColor();
+        }
+
+        public void LogSuccess(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            LogWithTime(message);
+            LogDatabase(message);
+            Console.ResetColor();
+        }
+
+        public void LogCreation(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            LogWithTime(message);
+            LogDatabase(message);
+            Console.ResetColor();
+        }
+
+        public void LogMessage(string message)
+        {
+            LogWithTime(message);
+        }
+
+        public void LogWithTime(string message)
+        {
+            Console.WriteLine(DateTime.Now.ToString("G") + message);
+        }
+
+        private void LogDatabase(string message)
+        {
+            databaseManager.InsertValue(tableName, columnsNames, new string[] { DateTime.Now.ToString("y/M/d h.m.ss"), message });
+        }
+
+        public void Setup()
+        {
+            DirectoryWatcher.OnSuccess += LogSuccess;
+            DirectoryWatcher.OnError += LogError;
+            DirectoryWatcher.OnFileTransfer += LogMessage;
+            DataManager.OnFileCreation += LogCreation;
+        }
+    }
+}
